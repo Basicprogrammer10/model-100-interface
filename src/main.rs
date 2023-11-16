94cf9e8a928c067887802cc497c08ecdfd627543
Connor Slade <connor@connorcode.com>
2023-11-15 21:35:29 -0500

    Working Decoder

--- src/main.rs
+++ src/main.rs
@@ -1,11 +1,16 @@
 use std::fs::{self, File};
 
-use bitvec::{order::Lsb0, slice::BitSlice, vec::BitVec};
+use bitvec::{
+    order::{Lsb0, Msb0},
+    slice::BitSlice,
+    vec::BitVec,
+};
 use hound::{self, WavReader, WavSpec, WavWriter};
 
 const CROSS_THRESHOLD: f32 = 0.1;
 const INT_CROSS_THRESHOLD: i32 = (CROSS_THRESHOLD * i16::MAX as f32) as i32;
 
+#[derive(Debug)]
 enum Pulse {
     Start,
     Zero,
@@ -32,20 +37,16 @@ fn main() {
     }
 
     let mut sections = Vec::new();
-    let mut dat = BitVec::<u8, Lsb0>::new();
+    let mut dat = Vec::new();
     // TODO: These are tied to the sample rate...
     for i in 0..intersections.len() - 1 {
         let diff = intersections[i + 1] - intersections[i];
         if (15..20).contains(&diff) {
-            dat.push(true);
+            dat.push(Pulse::One);
         } else if (35..39).contains(&diff) {
-            dat.push(false);
+            dat.push(Pulse::Zero);
         } else if (41..46).contains(&diff) {
-            // Start pulse should be on index where % 9 == 0
-            if dat.len() % 8 != 0 {
-                println!("Invalid start pulse index: {}", dat.len());
-                // dat.clear();
-            }
+            dat.push(Pulse::Start);
         } else if diff > 30000 {
             sections.push(dat);
             dat = Default::default();
@@ -54,74 +55,76 @@ fn main() {
         }
     }
 
-    let mut new_sections = Vec::new();
-    for section in sections {
-        let mut start = 30;
-        // Find leading 0x55 bytes
-        for (i, byte) in section.windows(8).enumerate() {
-            if byte_eq(byte, &[false, true, false, true, false, true, false, true]) {
-                start -= 1;
-            } else if start < 30 {
-                // new_sections.push(section[i..].to_bitvec());
-                new_sections.push(section.to_bitvec());
-                break;
+    {
+        let spec = WavSpec {
+            channels: 1,
+            bits_per_sample: 16,
+            ..spec
+        };
+        let mut debug = WavWriter::new(File::create("debug.wav").unwrap(), spec).unwrap();
+
+        let mut last = 0;
+        for i in intersections {
+            let gap = i - last - 1;
+            for _ in 0..gap {
+                debug.write_sample(0).unwrap();
+            }
+            last = i;
+
+            if (15..20).contains(&gap) {
+                debug.write_sample(i16::MAX).unwrap();
+            } else if (35..39).contains(&gap) {
+                debug.write_sample(i16::MIN).unwrap();
+            } else if (41..46).contains(&gap) {
+                debug.write_sample(i16::MAX / -2).unwrap();
+            } else if gap > 30000 {
+                debug.write_sample(i16::MAX / 2).unwrap();
+            } else {
+                panic!("Invalid pulse length: {}", gap);
             }
         }
+
+        debug.finalize().unwrap();
     }
 
-    let spec = WavSpec {
-        channels: 1,
-        bits_per_sample: 16,
-        ..spec
-    };
-    let mut debug = WavWriter::new(File::create("debug/out.wav").unwrap(), spec).unwrap();
-    let mut ones = WavWriter::new(File::create("debug/ones.wav").unwrap(), spec).unwrap();
-    let mut zero = WavWriter::new(File::create("debug/zero.wav").unwrap(), spec).unwrap();
-    let mut start = WavWriter::new(File::create("debug/start.wav").unwrap(), spec).unwrap();
+    let mut raw_sections = Vec::new();
+    let mut dat = BitVec::<u8, Msb0>::new();
+    for section in sections.iter_mut() {
+        let mut active = false;
+        for pulse in section {
+            match pulse {
+                Pulse::Zero => dat.push(false),
+                Pulse::One => dat.push(true),
+                Pulse::Start if active => assert!(dat.len() % 8 == 0),
+                Pulse::Start => dat.push(false),
+            }
 
-    let mut last = 0;
-    for i in intersections {
-        let gap = i - last - 1;
-        for _ in 0..gap {
-            debug.write_sample(0).unwrap();
-            ones.write_sample(0).unwrap();
-            zero.write_sample(0).unwrap();
-            start.write_sample(0).unwrap();
-        }
-        last = i;
-        debug.write_sample(i16::MAX).unwrap();
-
-        if (15..20).contains(&gap) {
-            ones.write_sample(i16::MAX).unwrap();
-        } else {
-            ones.write_sample(0).unwrap();
+            // If data equals [0, 1, 1, 1, 1, 1, 1, 1] then active = true
+            if !active
+                && dat.len() >= 8
+                && byte_eq(
+                    &dat[dat.len() - 8..],
+                    &[false, true, true, true, true, true, true, true],
+                )
+            {
+                active = true;
+                dat.clear();
+            }
         }
 
-        if (35..39).contains(&gap) {
-            zero.write_sample(i16::MAX).unwrap();
-        } else {
-            zero.write_sample(0).unwrap();
-        }
-
-        if (41..46).contains(&gap) {
-            start.write_sample(i16::MAX).unwrap();
-        } else {
-            start.write_sample(0).unwrap();
-        }
+        assert!(active, "Didn't find start sequence");
+        raw_sections.push(dat);
+        dat = Default::default();
     }
 
-    debug.finalize().unwrap();
-    ones.finalize().unwrap();
-    zero.finalize().unwrap();
-    start.finalize().unwrap();
-
-    for (i, section) in new_sections.iter().enumerate() {
+    for (i, section) in raw_sections.iter().enumerate() {
         fs::write(format!("out/section-{i}.bin"), section.as_raw_slice()).unwrap();
     }
 }
 
-fn byte_eq(a: &BitSlice<u8>, b: &[bool]) -> bool {
+fn byte_eq(a: &BitSlice<u8, Msb0>, b: &[bool]) -> bool {
     if a.len() != b.len() {
+        panic!("Length mismatch");
         return false;
     }
 

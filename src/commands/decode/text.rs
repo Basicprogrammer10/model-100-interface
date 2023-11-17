bb27332e5f2c1ef1ff29b89c49e290c878cbbd0e
Connor Slade <connor@connorcode.com>
2023-11-16 21:36:31 -0500

    Fix text parser

--- src/commands/decode/text.rs
+++ src/commands/decode/text.rs
@@ -3,7 +3,7 @@ use std::{
     io::{BufWriter, Write},
 };
 
-use anyhow::{bail, ensure, Result};
+use anyhow::{ensure, Context, Result};
 
 use crate::{
     args::Decode,
@@ -21,13 +21,9 @@ pub fn decode(samples: &[i32], spec: Spec, args: Decode) -> Result<()> {
 
     let mut sections = Vec::new();
     for i in 1..bin.len() {
-        let section = TextSection::parse(&bin[i].as_raw_slice())?;
-        let end = section.end;
+        let section =
+            TextSection::parse(&bin[i].as_raw_slice()).with_context(|| format!("Section {i}"))?;
         sections.push(section);
-
-        if end {
-            break;
-        }
     }
 
     let out = File::create(args.output)?;
@@ -49,20 +45,16 @@ struct TextHeader {
 
 struct TextSection<'a> {
     data: &'a [u8],
-    end: bool,
 }
 
 impl TextHeader {
     fn parse(bin: &[u8]) -> Result<Self> {
         ensure!(bin.len() == 38, "Invalid header length");
         let mut parser = BinParser::new(bin);
-        ensure!(parser.read_u8() == 0x9C, "Missing start byte");
+        ensure!(parser.read_u8() == 0x9C, "Non-text file");
 
         let name = [0; 6].map(|_| parser.read_u8() as char);
         let name_len = name.iter().position(|&c| c == ' ').unwrap_or(6) as u8;
-
-        parser.skip(2);
-        ensure!(parser.read_u8() == 0x74, "Non-text file");
 
         Ok(TextHeader { name, name_len })
     }
@@ -78,12 +70,6 @@ impl<'a> TextSection<'a> {
         let mut parser = BinParser::new(bin);
         ensure!(parser.read_u8() == 0x8D, "Missing start byte");
 
-        let end = match parser.get(0x101) {
-            0xD1 => false,
-            0x38 => true,
-            _ => bail!("Invalid end byte"),
-        };
-
         let end_pos = bin
             .iter()
             .take(0x100)
@@ -92,6 +78,6 @@ impl<'a> TextSection<'a> {
         ensure!(end_pos > 0, "Invalid end position");
         let data = &bin[0x1..=end_pos];
 
-        Ok(TextSection { data, end })
+        Ok(TextSection { data })
     }
 }
